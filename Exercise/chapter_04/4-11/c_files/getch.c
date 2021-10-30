6a9a7e7bfca02466ee2dda999de6d1e731fe03df
Ian <hippo032318@gmail.com>
2021-10-30 19:28:53 +0800

    Learning record - K&R C

---
+++ Exercise/chapter_04/4-11/c_files/getch.c
@@ -0,0 +1,39 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  getch.c
+ *
+ *    Description:  Get a character and push back the remain character.
+ *
+ *        Version:  1.0
+ *        Created:  2021年10月07日 23時15分21秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Ian 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+#define BUFSIZE 100
+
+char buf[BUFSIZE];     /*  buffer for ungetch  */
+int bufp = 0; /* next free position in buf */
+
+int getch(void){   /*  get a (possibly pushed back) character */
+
+	return (bufp > 0) ? buf[--bufp]: getchar(); 
+}
+
+
+void ungetch(int c){ /*  push character back on input */
+
+	if(bufp >= BUFSIZE){
+		printf("ungetch : too many characters.\n");
+	}else{
+		buf[bufp++] = c;
+	}
+}
+
