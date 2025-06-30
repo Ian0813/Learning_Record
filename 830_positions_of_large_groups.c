744e5ca31d670e6ec31d2e60d0f53cebef34783a
Ian Chen <ianchen.twi@gmail.com>
2025-06-30 22:33:54 +0800

    Progress on LeetCode:
      830_positions_of_large_groups.c
    
    Signed-off-by: Ian Chen <ianchen.twi@gmail.com>

--- 830_positions_of_large_groups.c
+++ 830_positions_of_large_groups.c
@@ -15,65 +15,139 @@
 #include <string.h>
 #include <time.h>
 
-#define MAX 1000
+typedef enum {false, true} bool;
+
+#define MAX_ITEMS 26
+#define PAIR_LEN 2
+#define QUALIFY_LEN 3
+
+enum {
+    S_INDEX = 0,
+    E_INDEX = 1
+};
+
+typedef struct stack {
+    int top;
+    char *buffer;
+    int size;
+} Stack;
+
+Stack *stack_init(int size) {
+
+    Stack *obj = NULL;
+
+    if (size) {
+        obj = (Stack *) malloc(sizeof(*obj));
+        obj->size = size;
+        obj->buffer = (char *) calloc(size, sizeof(char));
+        obj->top = 0;
+    }
+    return obj;
+}
+
+bool stack_push(Stack *obj, char data) {
+
+    if (!obj || obj->top >= obj->size)
+        return false;
+
+    obj->buffer[obj->top++] = data;
+
+    return true;
+}
+
+bool stack_pop(Stack *obj) {
+
+    if (!obj || !obj->top) {
+        return false;
+    }
+
+    obj->top--;
+    return true;
+}
+
+char *stack_peek(Stack *obj) {
+
+    if (!obj || !obj->top)
+        return NULL;
+
+    return obj->buffer + (obj->top-1);
+}
+
+void stack_delete(Stack *obj) {
+
+    if (obj) {
+        free(obj->buffer);
+        free(obj);
+    }
+    return;
+}
+
+static int **allocate_result(int rlen, int clen) {
+
+    int **ptr = NULL;
+
+    ptr = (int **) calloc(rlen, sizeof(int *));
+
+    for (int i = 0; i < rlen; i++) {
+        ptr[i] = (int *) calloc(clen, sizeof(int));
+    }
+
+    return ptr;
+}
+
+static int *allocate_column_size(int len) {
+
+    int *ptr = NULL;
+
+    if (len) {
+        ptr = (int *) calloc(len, sizeof(int));
+
+        for (int i = 0; i < len; i++) {
+            ptr[i] = PAIR_LEN;
+        }
+    }
+
+    return ptr;
+}
 
 int** largeGroupPositions(char* s, int* returnSize, int** returnColumnSizes) {
 
-    int i = 0, j = 0, index = 0;
-    int **group = NULL;
+    int count = 0, findex = 0;
+    int **group = NULL, glen = 0;
+    Stack *obj = NULL;
 
-    group = (int **) calloc(MAX, sizeof(int *));
+    group = allocate_result(BUFSIZ, PAIR_LEN);
+    obj = stack_init(BUFSIZ);
 
-    for (index = 0; index < MAX; index++) {
-        group[index] = (int *) calloc(2, sizeof(int));
+    stack_push(obj, s[0]);
+    findex = 0;
+    count++;
+
+    for (int i = 1; i < strlen(s); i++) {
+
+        if (s[i] == *stack_peek(obj)) {
+            count++;
+        } else {
+            if (count >= QUALIFY_LEN) {
+                group[glen][S_INDEX] = findex;
+                group[glen][E_INDEX] = i-1;
+                glen++;
+            }
+            stack_push(obj, s[i]);
+            findex = i;
+            count = 1;
+        }
     }
-	index = 0;
 
+    if (count >= QUALIFY_LEN) {
+        group[glen][S_INDEX] = findex;
+        group[glen][E_INDEX] = strlen(s)-1;
+        glen++;
+    }
 
-    for (; i < strlen(s); i++) {
-        for (; j < strlen(s); j++) {
-            if (s[i] != s[j]) {
-                if ((j - i) > 2) {
-                    group[index][0] = i;
-                    group[index][1] = j-1;
-					index++;
-					i = j - 1;
-                }
-				break;
-			}
-		}
-	}
-
-    *returnSize = index;
-	**returnColumnSizes = 2;
+    *returnSize = glen;
+    *returnColumnSizes = allocate_column_size(glen);
+    stack_delete(obj);
 
     return group;
-}
-
-void release(int **group) {
-
-    for (int index = 0; index < MAX; index++) {
-        free(*(group+index));
-	}
-	free(group);
-
-	return;
-}
-
-int main(void) {
-
-    char *s = "abcdddeeeeaabbbcd";
-	int row = 0, column = 0;
-	int *ptr = &column;
-	int **group = NULL; 
-
-	group = largeGroupPositions(s, &row, &ptr);
-
-	for (int i = 0; i < row; i++) {
-        printf("%d, %d\n", group[i][0], group[i][1]);
-	}
-
-	release(group);
-
-	return 0;
 }
